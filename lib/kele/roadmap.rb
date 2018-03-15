968c08497d365d120714e36e52b2beee3e43c108
damianrivas <drivas1993@gmail.com>
2018-03-15 22:39:26 +0000

    Add ability to obtain remaining checkpoints

--- lib/kele/roadmap.rb
+++ lib/kele/roadmap.rb
@@ -27,4 +27,17 @@ module Roadmap
     
     JSON.parse(self.class.get(url, options).body)
   end
+  
+  def get_remaining_checkpoints(chain_id)
+    url = @base_url + "/enrollment_chains/#{chain_id}/checkpoints_remaining_in_section"
+    
+    options = {
+      headers: {
+        content_type: 'application/json',
+        authorization: @auth_token
+      }
+    }
+    
+    JSON.parse(self.class.get(url, options).body)
+  end
 end
